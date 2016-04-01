cd5c9d6e5f61e21249fcb8d1fe86146ace2040b1
TuanPA <phamanhtuan206@gmail.com>
2016-04-01 22:35:24 +0700

    no message

--- ex5/10954.cpp
+++ ex5/10954.cpp
@@ -9,36 +9,78 @@ using namespace std;
 int nInput[100];
 int n;
 
-void input(){
-    for(int i = 0; i < n; i++){
+void bubbleSort()
+{
+    int i, j, flag = 1;
+    int temp;
+    int numLength = n;
+
+    while(flag == 1)
+    {
+        flag = 0;
+        for (j=0; j < (numLength - 1); j++)
+        {
+            if (nInput[j] > nInput[j+1] )
+            {
+                temp = nInput[j];
+                nInput[j] = nInput[j+1];
+                nInput[j+1] = temp;
+                flag = 1;
+            }
+        }
+    }
+    return;
+}
+
+void input()
+{
+    printf("n : %d\n",n);
+    for(int i = 0; i < n; i++)
+    {
         scanf("%d",&nInput[i]);
     }
 }
-void handle(){
-    int sum = nInput[0];
+void handle()
+{
+    bubbleSort();
+    int sum = 0;
     int result = 0;
-    for(int i = 1; i < n; i++){
-        sum+= nInput[i] ;
-
-        result+= sum;
+    for(int i = 0; i < n; i++)
+    {
+        if(i  == 0){
+            sum += nInput[i];
+        }else{
+            sum+= nInput[i] ;
+            result+= sum;
+        }
     }
     printf("%d",result);
 }
 
-int main(){
-	freopen("10954.inp","r",stdin);
-	int x = 0;
-	while(scanf("%d",&n) > 0){
-	    if(n > 0){
-            if(x > 0){
+
+int main()
+{
+    freopen("10954.inp","r",stdin);
+    int x = 0;
+    while(true)
+    {
+        scanf("%d",&n);
+        if(n > 0)
+        {
+            if(x > 0)
+            {
                 printf("\n");
             }
             input();
             handle();
             x++;
-	    }
-	}
+        }
+        else
+        {
+            break;
+        }
+    }
 
-	return 0;
+    return 0;
 }
 
